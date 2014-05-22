6dd608dfb46984e08f34f6f4c3c5995737f00851
Barnaby Court <bcourt@redhat.com>
2014-05-22 10:06:44 -0400

    Fix direct reference to the working directory that was not converted to use the get method

--- plugins/pulp_docker/plugins/distributors/publish_steps.py
+++ plugins/pulp_docker/plugins/distributors/publish_steps.py
@@ -67,7 +67,7 @@ class ExportPublisher(PublishStep):
         self.add_child(PublishImagesStep())
         tar_file = os.path.join(configuration.get_export_repo_directory(config),
                                 configuration.get_export_repo_filename(repo, config))
-        self.add_child(SaveTarFilePublishStep(self.working_dir, tar_file))
+        self.add_child(SaveTarFilePublishStep(self.get_working_dir(), tar_file))
 
 
 class PublishImagesStep(UnitPublishStep):
