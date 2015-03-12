25cb7fbe228d2062f3a651097b4c56eef62a8e4a
Barnaby Court <bcourt@redhat.com>
2015-03-12 09:44:38 -0400

    Bump version to release candidate

--- common/setup.py
+++ common/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp_docker_common',
-    version='1.0.0b2',
+    version='1.0.0c3',
     packages=find_packages(),
     url='http://www.pulpproject.org',
     license='GPLv2+',
