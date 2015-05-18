cc728f868895f8c97d2dc20f59124c384250aa12
Dennis Kliban <dkliban@redhat.com>
2015-05-18 14:57:54 -0400

    Bumping version

--- plugins/setup.py
+++ plugins/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp_docker_plugins',
-    version='1.0.1a1',
+    version='1.0.1b2',
     packages=find_packages(),
     url='http://www.pulpproject.org',
     license='GPLv2+',
--- pulp-docker.spec
+++ pulp-docker.spec
@@ -3,7 +3,7 @@
 
 Name: pulp-docker
 Version: 1.0.1
-Release: 0.1.alpha%{?dist}
+Release: 0.2.beta%{?dist}
 Summary: Support for Docker layers in the Pulp platform
 Group: Development/Languages
 License: GPLv2
