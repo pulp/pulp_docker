16bd2327b066e32eb5534747f73e61094cbc12f4
Dennis Kliban <dkliban@redhat.com>
2016-01-15 15:35:11 -0500

    Bumping version to 2.0.0-0.3.beta

--- plugins/setup.py
+++ plugins/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp_docker_plugins',
-    version='2.0.0b2',
+    version='2.0.0b3',
     packages=find_packages(),
     url='http://www.pulpproject.org',
     license='GPLv2+',
