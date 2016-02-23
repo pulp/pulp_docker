bd8a79460dbd626709445641ebe5878a3e5f695c
Dennis Kliban <dkliban@redhat.com>
2016-02-23 11:16:34 -0500

    Bumping version to 2.0.0-0.6.beta

--- common/setup.py
+++ common/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp_docker_common',
-    version='2.0.0b5',
+    version='2.0.0b6',
     packages=find_packages(exclude=['test']),
     url='http://www.pulpproject.org',
     license='GPLv2+',
