568a21ec68dfb2137618b2ea1f16e5bfaf715c40
Sean Myers <sean.myers@redhat.com>
2016-06-27 10:47:16 -0400

    Bumping version to 2.0.2-1

--- common/setup.py
+++ common/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp_docker_common',
-    version='2.0.2b3',
+    version='2.0.2',
     packages=find_packages(exclude=['test']),
     url='http://www.pulpproject.org',
     license='GPLv2+',
