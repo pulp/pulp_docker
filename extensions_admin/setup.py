9caef51985aaf0a4a828cd24349789ac64fcd392
Sean Myers <sean.myers@redhat.com>
2017-01-17 17:00:00 -0500

    Bumping version to 2.3.0-0.1.beta

--- extensions_admin/setup.py
+++ extensions_admin/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp_docker_extensions_admin',
-    version='2.3a1',
+    version='2.3b1',
     packages=find_packages(exclude=['test']),
     url='http://www.pulpproject.org',
     license='GPLv2+',
