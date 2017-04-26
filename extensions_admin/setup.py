52837b6f186604649872a46ae6d35f08182f0c57
Patrick Creech <pcreech@redhat.com>
2017-04-26 15:39:52 -0400

    Bumping version to 2.4.0-1

--- extensions_admin/setup.py
+++ extensions_admin/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp_docker_extensions_admin',
-    version='2.4c2',
+    version='2.4',
     packages=find_packages(exclude=['test']),
     url='http://www.pulpproject.org',
     license='GPLv2+',
