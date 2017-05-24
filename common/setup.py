44a13d05dcbbbf8895c11bfb6048c14f1c65d96f
werwty <bihan.zh@gmail.com>
2017-05-24 15:06:10 -0400

    Bumping version to 2.5.0-0.1.alpha

--- common/setup.py
+++ common/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp_docker_common',
-    version='2.4a1',
+    version='2.5a1',
     packages=find_packages(exclude=['test']),
     url='http://www.pulpproject.org',
     license='GPLv2+',
--- extensions_admin/setup.py
+++ extensions_admin/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp_docker_extensions_admin',
-    version='2.4a1',
+    version='2.5a1',
     packages=find_packages(exclude=['test']),
     url='http://www.pulpproject.org',
     license='GPLv2+',
