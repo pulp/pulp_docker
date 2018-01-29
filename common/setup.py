df7b5aaa094f4b24f2f4c083caa309b49254fcff
Patrick Creech <pcreech@redhat.com>
2018-01-29 17:18:19 -0500

    Update version to 3.1.1
    Update versoin to 3.1.1

--- common/setup.py
+++ common/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp_docker_common',
-    version='3.1.1b2',
+    version='3.1.1',
     packages=find_packages(exclude=['test']),
     url='http://www.pulpproject.org',
     license='GPLv2+',
