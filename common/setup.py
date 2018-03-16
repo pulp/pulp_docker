ee86414b31dd6717e92dabdba256c99c22c8d650
Eric D. Helms <ericdhelms@gmail.com>
2018-03-16 11:34:01 -0400

    Updating version to 3.1.2

--- common/setup.py
+++ common/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp_docker_common',
-    version='3.1.2b1',
+    version='3.1.2',
     packages=find_packages(exclude=['test']),
     url='http://www.pulpproject.org',
     license='GPLv2+',
