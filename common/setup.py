7e0a7db1eea8a6bc534caa672b7f47db031a0688
Patrick Creech <pcreech@redhat.com>
2018-08-10 10:17:43 -0400

    Pulp Docker 3.2.0 Beta 1

--- common/setup.py
+++ common/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp_docker_common',
-    version='3.2a1',
+    version='3.2b1',
     packages=find_packages(exclude=['test']),
     url='http://www.pulpproject.org',
     license='GPLv2+',
