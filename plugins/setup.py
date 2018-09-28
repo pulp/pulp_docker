b17843a24ebf8761df183ccb63d202580e328321
Patrick Creech <pcreech@redhat.com>
2018-09-28 11:46:48 -0400

    Pulp Docker 3.4.0 Alpha

--- plugins/setup.py
+++ plugins/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp_docker_plugins',
-    version='3.3a1',
+    version='3.4a1',
     packages=find_packages(exclude=['test', 'test.*']),
     url='http://www.pulpproject.org',
     license='GPLv2+',
