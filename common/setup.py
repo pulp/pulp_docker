e232b9b713366e862d892e23b9dbd8df77f52f4b
Zjhuntin <zhunting@redhat.com>
2018-10-02 13:54:38 -0400

    Pulp-Docker 3.2.1

--- common/setup.py
+++ common/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp_docker_common',
-    version='3.2.1b1',
+    version='3.2.1',
     packages=find_packages(exclude=['test']),
     url='http://www.pulpproject.org',
     license='GPLv2+',
--- extensions_admin/setup.py
+++ extensions_admin/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp_docker_extensions_admin',
-    version='3.2.1b1',
+    version='3.2.1',
     packages=find_packages(exclude=['test']),
     url='http://www.pulpproject.org',
     license='GPLv2+',
