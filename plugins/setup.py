8edece8a13f609b120c194551bdb179c395aa0f7
Patrick Creech <pcreech@redhat.com>
2018-12-04 09:08:02 -0500

    Pulp Docker 3.2.2 GA

--- plugins/setup.py
+++ plugins/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp_docker_plugins',
-    version='3.2.2c1',
+    version='3.2.2',
     packages=find_packages(exclude=['test', 'test.*']),
     url='http://www.pulpproject.org',
     license='GPLv2+',
