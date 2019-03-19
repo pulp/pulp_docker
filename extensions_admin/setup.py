84f1a204f71c97c4746434b7215c24977497a5c3
Patrick Creech <pcreech@redhat.com>
2019-03-18 22:06:15 -0400

    Pulp Docker 3.2.3 RC 1

--- extensions_admin/setup.py
+++ extensions_admin/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp_docker_extensions_admin',
-    version='3.2.2',
+    version='3.2.3c1',
     packages=find_packages(exclude=['test']),
     url='http://www.pulpproject.org',
     license='GPLv2+',
