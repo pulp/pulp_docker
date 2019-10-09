0dc99ebd8c318b6abebea379902269aa699cd3ed
Ina Panova <ipanova@redhat.com>
2019-10-09 22:21:55 +0200

    Fix image tagging script.
    [noissue]

--- docs/_scripts/remote.sh
+++ docs/_scripts/remote.sh
@@ -3,7 +3,7 @@ echo "Creating a remote that points to an external source of container images."
 http POST $BASE_ADDR/pulp/api/v3/remotes/docker/docker/ \
     name='my-hello-repo' \
     url='https://registry-1.docker.io' \
-    upstream_name='library/hello-world'
+    upstream_name='pulp/test-fixture-1'
 
 echo "Export an environment variable for the new remote URI."
 export REMOTE_HREF=$(http $BASE_ADDR/pulp/api/v3/remotes/docker/docker/ \
