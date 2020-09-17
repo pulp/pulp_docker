737543ea0812833d5fa637b3899982fafb67cbe1
Evgeni Golov <evgeni@golov.de>
2020-09-17 12:56:16 +0200

    pulp_docker 3.2.7

--- plugins/setup.py
+++ plugins/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp_docker_plugins',
-    version='3.2.6',
+    version='3.2.7',
     packages=find_packages(exclude=['test', 'test.*']),
     url='http://www.pulpproject.org',
     license='GPLv2+',
